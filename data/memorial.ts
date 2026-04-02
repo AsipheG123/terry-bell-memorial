e11591b16384de7ae11fa114b78dba9fcfdfc5e5
AsipheG123 <asiphe4g@gmail.com>
2026-04-02 14:50:44 +0200

    Update memorial service section

--- data/memorial.ts
+++ data/memorial.ts
@@ -1,11 +1,11 @@
 export const memorialService = {
   heading: "Keep Remembering",
-  date: "To be announced",
+  date: "Friday, 8 May 2026",
   time: "To be announced",
-  venue: "To be announced",
-  address: "To be announced",
-  directionsUrl: "https://maps.google.com/?q=45+Rosebank+Avenue+Parktown+Johannesburg",
+  venue: "Masque Theatre, Muizenberg",
+  address: "37 Main Rd, Muizenberg, Cape Town",
+  directionsUrl: "https://www.google.com/maps/dir//The+Masque+Theatre,+37+Main+Rd,+Muizenberg,+Cape+Town,+7945/@-34.102722,18.4659471,17z/data=!4m17!1m7!3m6!1s0x1dcc405350492dcd:0x7c0418166ba39202!2sThe+Masque+Theatre!8m2!3d-34.102722!4d18.468522!16s%2Fg%2F12vt1srtk!4m8!1m0!1m5!1m1!1s0x1dcc405350492dcd:0x7c0418166ba39202!2m2!1d18.468522!2d-34.102722!3e0?entry=ttu&g_ep=EgoyMDI2MDMzMC4wIKXMDSoASAFQAw%3D%3D",
   description:
-    "We continue to remember Terry Bell with love, respect and gratitude. Memorial details will be communicated on this page soon. In the meantime, please share a memory or tribute using the Public Tributes section below.",
+    "We continue to remember Terry Bell with love, respect and gratitude. The memorial service will be held at the Masque Theatre, Muizenberg on Friday, 8 May 2026. In the meantime, please share a memory or tribute using the Public Tributes section below.",
   note: "A private committal was held in accordance with Terry's wishes. He donated his body to medical science.",
 };
