3101cd83f7ccb8a24563e877ce42c65a753c0c06
AsipheG123 <asiphe4g@gmail.com>
2026-04-07 14:34:12 +0200

    Add time of service

--- data/memorial.ts
+++ data/memorial.ts
@@ -1,7 +1,7 @@
 export const memorialService = {
   heading: "Keep Remembering",
   date: "Friday, 8 May 2026",
-  time: "To be announced",
+  time: "15:00 SA Time (GMT+2)",
   venue: "Masque Theatre, Muizenberg",
   address: "37 Main Rd, Muizenberg, Cape Town",
   directionsUrl: "https://www.google.com/maps/dir//The+Masque+Theatre,+37+Main+Rd,+Muizenberg,+Cape+Town,+7945/@-34.102722,18.4659471,17z/data=!4m17!1m7!3m6!1s0x1dcc405350492dcd:0x7c0418166ba39202!2sThe+Masque+Theatre!8m2!3d-34.102722!4d18.468522!16s%2Fg%2F12vt1srtk!4m8!1m0!1m5!1m1!1s0x1dcc405350492dcd:0x7c0418166ba39202!2m2!1d18.468522!2d-34.102722!3e0?entry=ttu&g_ep=EgoyMDI2MDMzMC4wIKXMDSoASAFQAw%3D%3D",
